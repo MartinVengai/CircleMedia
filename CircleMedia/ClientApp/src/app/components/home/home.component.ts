9c5bbe834c29fd322da694018ca450b12c1e55b4
Martin Vengai <martvee97@gmail.com>
2018-07-29 07:10:33 +0200

    Add fullcalendar

--- CircleMedia/ClientApp/src/app/components/home/home.component.ts
+++ CircleMedia/ClientApp/src/app/components/home/home.component.ts
@@ -4,6 +4,8 @@ import { ProjectService } from './../../services/project.service';
 import { Component, OnInit } from '@angular/core';
 import { fadeInOut } from '../../services/animations';
 import { IProjectStatistics } from '../../models/IProjectStatistics';
+import 'fullcalendar';
+declare let $: any;
 
 @Component({
   selector: 'app-home',
@@ -18,6 +20,13 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
+    $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today ',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      }
+    });
     this.projectService.getProjectStatistics(this.currentUserId).subscribe(result => this.projectStatistics = result);
   }
 
--- CircleMedia/ClientApp/src/main.ts
+++ CircleMedia/ClientApp/src/main.ts
@@ -1,13 +1,11 @@
-// ====================================================
-// More Templates: https://www.ebenmonney.com/templates
-// Email: support@ebenmonney.com
-// ====================================================
-
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+import * as jQuery from 'jquery';
+(window as any).$ = (window as any).jQuery = jQuery;
 
 if (environment.production) {
   enableProdMode();
